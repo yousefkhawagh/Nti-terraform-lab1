be4591852977456e51c16121c6428f24110bcbef
Yousef Ashraf <140858931+yousefkhawagh@users.noreply.github.com>
2025-12-18 14:45:44 +0200

    Change S3 backend region to 'us-east-1'

--- lab2/backend.tf
+++ lab2/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "nti-test-terraform"
     key            = "envs/dev/terraform.tfstate"
-    region         = var.region
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
